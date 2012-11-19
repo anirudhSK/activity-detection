451beede4f2a45e8f992d22b55c7decba2f58044
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-11-18 20:51:39 -0500

    Bug fix to stitch-traces.py

--- src/stitch-traces.py
+++ src/stitch-traces.py
@@ -3,18 +3,19 @@ from markov import *
 from trace import *
 ''' Program to stitch traces together into a larger trace '''
 import sys
-if ( len(sys.argv) < 7) :
-	print "Usage : python markov-chain.py duration static_trace walking_trace running_trace biking_trace driving_trace"
+if ( len(sys.argv) < 8) :
+	print "Usage : python markov-chain.py duration static_trace walking_trace running_trace biking_trace driving_trace random_seed "
 	exit(5)
 else :
-	m=MarkovChain()
 	duration=int(sys.argv[1]);
-	sampled_dtmc=m.simulate(duration);
 	static_trace =Trace(sys.argv[2])
 	walking_trace=Trace(sys.argv[3])
 	running_trace=Trace(sys.argv[4])
 	biking_trace =Trace(sys.argv[5])
 	driving_trace=Trace(sys.argv[6])
+	random_seed=int(sys.argv[7]);
+	m=MarkovChain(random_seed)
+	sampled_dtmc=m.simulate(duration);
 	for i in range(0,len(sampled_dtmc)) :
 		current=sampled_dtmc[i]
 		if ( i < len(sampled_dtmc) -1 ) :
