7167bb2981624003de1f9b212f814b74401ed14a
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-11-29 13:08:00 -0500

    Precompute to speed up kde implementation

--- utils/kde.py
+++ utils/kde.py
@@ -10,18 +10,22 @@ x = linspace(min(feature_list),max(feature_list),300)
 
 ''' trunc_pdf 60% slower than scipy.stats.gaussian_kde '''
 bw=0.003
-def trunc_pdf (x,samples) :
-	x_repeated=array([x]*len(samples))
-	norm_pdfs=norm.pdf((x_repeated-samples)/bw)/(norm.sf((-samples)/bw))
-	return numpy.sum(norm_pdfs)/(bw*len(samples))
-plt.plot(x,map(lambda y : trunc_pdf(y,array(feature_list)), x ),'r')
+samples=array(feature_list)
+sf_array=(norm.sf((-samples)/bw))
+norm_constant=bw*len(samples)
+sample_length=len(samples)
+def trunc_pdf (x) :
+	x_repeated=array([x]*sample_length)
+	norm_pdfs=norm.pdf((x_repeated-samples)/bw)/sf_array
+	return numpy.sum(norm_pdfs)/norm_constant
+plt.plot(x,map(lambda y : trunc_pdf(y), x ),'r')
 
 ''' untrunc_pdf 30% slower than scipy.stats.gaussian_kde '''
-def untrunc_pdf (x,samples) :
-	x_repeated=array([x]*len(samples))
+def untrunc_pdf (x) :
+	x_repeated=array([x]*sample_length)
 	norm_pdfs=norm.pdf((x_repeated-samples)/bw)
-	return numpy.sum(norm_pdfs)/(bw*len(samples))
-plt.plot(x,map(lambda y : untrunc_pdf(y,array(feature_list)), x ),'g')
+	return numpy.sum(norm_pdfs)/norm_constant
+plt.plot(x,map(lambda y : untrunc_pdf(y), x ),'g')
 
 ''' scipy.stats.gaussian_kde '''
 kernel_pdf = gaussian_kde(feature_list)
