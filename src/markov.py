bfa329ea79c5c24e226ceed69ac388030275ad9f
Anirudh Sivaraman <sk.anirudh@gmail.com>
2012-12-05 23:40:19 -0500

    Fixed bug in stitch-traces.py thanks to which the average dwell time per state was about 2 mins ! Ref. : www.math.wisc.edu/~anderson/605F11/Notes/StochBioChapter6.pdf

--- src/markov.py
+++ src/markov.py
@@ -4,33 +4,24 @@ from random import *
 from math import *
 class MarkovChain(object) :
 	states=[] 		# list of states id'ed by name
-	transition_rates=[] 	# list of lists with transition rates per second
 	def __init__ (self,num) :
 		seed(num)	
 		self.states=['static','walking','running','biking','driving']
-		for i in range(0,len(self.states)) :
-			self.transition_rates.append([])
-			for j in range(0,len(self.states)) :
-				self.transition_rates[i].append(1/600000.0); # one transition every 600000 ms
 	def simulate(self,duration) :
 		time=0;
 		''' initially, pick state with a uniform prior '''
-		state = min(int(random() * 5),5)
+		state = min(int(random() * 5),5) # sample between 0 and 4 uniformly
 		sampled_dtmc=[]
 		while ( time < duration ) :
 			''' generate next state using all possible jumps from here '''
 			sampled_dtmc+=[(time,state)]
-			min_time=-1
-			for j in range(0,len(self.states)):
-				transition_time=self.sample_exp(self.transition_rates[state][j])
-				if (min_time == -1 ) :
-					min_time=transition_time
-					target_state=j
-				elif (transition_time < min_time) :
-					min_time=transition_time
-					target_state=j		
+			transition_time=self.sample_exp(1/600000.0); # 1 per 10 min or 1 /600000 sec -1
+			# pick next_state at random
+			next_state=min(int(random() * 4),4) # sample between 0 and 3 uniformly
+			target_state=(state+1+next_state)%5 # cyclic addition
+			assert(target_state!=state)
 			state=target_state
-			time=time+min_time
+			time+=transition_time
 		return sampled_dtmc	
 	def sample_exp(self,rate) :
 		return -log(random())/rate;	
