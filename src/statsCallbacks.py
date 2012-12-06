9d0ddaec31864ccd193833c556006662fc403965
Tiffany Chen <yuhan210@gmail.com>
2012-12-06 16:37:58 -0500

    minor bug fixed

--- src/statsCallbacks.py
+++ src/statsCallbacks.py
@@ -98,5 +98,5 @@ class CallbackStats(object) :
 				binned_time_series[bin_index]=[ml_estimate]
 			else :
 				if ml_estimate not in binned_time_series[bin_index] :
-					binned_time_series[bin_index]+=ml_estimate
+					binned_time_series[bin_index]+=[ml_estimate]
 		return binned_time_series
