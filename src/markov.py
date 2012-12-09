37ca98da6666dd5a760284f860e6fdd973b4a4ca
Tiffany Chen <yuhan210@gmail.com>
2012-12-08 20:14:12 -0500

    generate/classify traces without biking (usgin accel only)

--- src/markov.py
+++ src/markov.py
@@ -4,21 +4,21 @@ from random import *
 from math import *
 class MarkovChain(object) :
 	states=[] 		# list of states id'ed by name
-	def __init__ (self,num,states) :
+	def __init__ (self,num) :
 		seed(num)	
-		self.states=states
+		self.states=['static','walking','running','driving']
 	def simulate(self,duration) :
 		time=0;
 		''' initially, pick state with a uniform prior '''
-		state = min(int(random() * len(self.states)),len(self.states)) # sample between 0 and 4 uniformly
+		state = min(int(random() * 4),4) # sample between 0 and 4 uniformly
 		sampled_dtmc=[]
 		while ( time < duration ) :
 			''' generate next state using all possible jumps from here '''
 			sampled_dtmc+=[(time,state)]
-			transition_time=self.sample_exp(1/600000.0); # 1 per 10 min or 1 /600000 sec -1
+			transition_time=self.sample_exp(1/900000.0); # 1 per 10 min or 1 /600000 sec -1
 			# pick next_state at random
-			next_state=min(int(random() * (len(self.states)-1)),(len(self.states)-1)) # sample between 0 and 3 uniformly
-			target_state=(state+1+next_state)%len(self.states) # cyclic addition
+			next_state=min(int(random() * 3),3) # sample between 0 and 3 uniformly
+			target_state=(state+1+next_state)%4# cyclic addition
 			assert(target_state!=state)
 			state=target_state
 			time+=transition_time
